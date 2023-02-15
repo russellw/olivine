e714c8bc18b9d7d08764345100c33a0e961aa1e7
Russell Wallace <russell.wallace@gmail.com>
2023-02-15 21:17:36 +0000

    param

--- etc/test_prover.bat
+++ etc/test_prover.bat
@@ -1,4 +1,4 @@
 javac -d %tmp% --enable-preview -source 18 C:\olivine\src\*.java
 if %errorlevel% neq 0 goto :eof
 
-python C:\olivine\etc\test_prover.py "java -Xmx20g -cp %tmp% -ea --enable-preview Prover -t=60" %*
+python C:\olivine\etc\test_prover.py "java -Xmx20g -cp %tmp% -ea --enable-preview Prover" %*
