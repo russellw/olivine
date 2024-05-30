636e8f8254edb2a7a8e819d9ceb105274062b766
Russell Wallace <russell.wallace@gmail.com>
2024-05-30 03:19:54 +0100

    Block

--- src/main/java/olivine/Block.java
+++ src/main/java/olivine/Block.java
@@ -1,3 +1,18 @@
 package olivine;
 
-public final class Block {}
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public final class Block implements Iterable<Instruction> {
+  private final List<Instruction> instructions = new ArrayList<>();
+
+  @Override
+  public Iterator<Instruction> iterator() {
+    return instructions.iterator();
+  }
+
+  void add(Instruction instruction) {
+    instructions.add(instruction);
+  }
+}
