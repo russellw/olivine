cf80d00624dd28a3c072494e1362a10e9d7aa050
Russell Wallace <russell.wallace@gmail.com>
2024-05-31 02:34:53 +0100

    rename

--- src/main/java/olivine/Fn.java
+++ src/main/java/olivine/Fn.java
@@ -3,14 +3,14 @@ package olivine;
 import java.util.List;
 
 public final class Fn extends Global {
-  public final Type rtype;
+  public final Type returnType;
   public final List<Var> params;
   public final boolean varargs;
   public Block entry;
 
-  public Fn(String name, Type rtype, List<Var> params, boolean varargs) {
+  public Fn(String name, Type returnType, List<Var> params, boolean varargs) {
     super(name);
-    this.rtype = rtype;
+    this.returnType = returnType;
     this.params = params;
     this.varargs = varargs;
   }
