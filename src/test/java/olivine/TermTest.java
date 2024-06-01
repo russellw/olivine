74ceee78fe327603bf8be3e83fe47fa877435b28
Russell Wallace <russell.wallace@gmail.com>
2024-06-01 05:21:19 +0100

    test

--- src/test/java/olivine/TermTest.java
+++ src/test/java/olivine/TermTest.java
@@ -2,6 +2,10 @@ package olivine;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.math.BigInteger;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.Test;
 
 class TermTest {
@@ -67,5 +71,113 @@ class TermTest {
     Term r = null;
     for (var x : a) r = x;
     assertEquals(two, r);
+  }
+
+  @Test
+  void testIntConstantWithLong() {
+    Term term = Term.intConstant(Type.I1, 10L);
+    assertEquals("10", term.toString());
+    assertEquals(Type.I1, term.type());
+  }
+
+  @Test
+  void testIntConstantWithBigInteger() {
+    BigInteger value = new BigInteger("123456789");
+    Term term = Term.intConstant(Type.I32, value);
+    assertEquals("123456789", term.toString());
+    assertEquals(Type.I32, term.type());
+  }
+
+  @Test
+  void testFloatConstant() {
+    Term term = Term.floatConstant(Type.FLOAT, "3.14");
+    assertEquals("3.14", term.toString());
+    assertEquals(Type.FLOAT, term.type());
+  }
+
+  @Test
+  void testFNeg() {
+    Term term = Term.intConstant(Type.I1, 5L);
+    Term fneg = term.fneg();
+    assertEquals(Tag.FNEG, fneg.tag());
+    assertEquals(term, fneg.get(0));
+  }
+
+  @Test
+  void testAdd() {
+    Term term1 = Term.intConstant(Type.I1, 2L);
+    Term term2 = Term.intConstant(Type.I1, 3L);
+    Term add = term1.add(term2);
+    assertEquals(Tag.ADD, add.tag());
+    assertEquals(term1, add.get(0));
+    assertEquals(term2, add.get(1));
+  }
+
+  @Test
+  void testSelect() {
+    Term cond = Term.TRUE;
+    Term ifTrue = Term.intConstant(Type.I1, 1L);
+    Term ifFalse = Term.intConstant(Type.I1, 0L);
+    Term select = cond.select(ifTrue, ifFalse);
+    assertEquals(Tag.SELECT, select.tag());
+    assertEquals(cond, select.get(0));
+    assertEquals(ifTrue, select.get(1));
+    assertEquals(ifFalse, select.get(2));
+  }
+
+  @Test
+  void testCall() {
+    Term func = Term.intConstant(Type.I1, 1L);
+    Term arg1 = Term.intConstant(Type.I1, 2L);
+    Term arg2 = Term.intConstant(Type.I1, 3L);
+    Term call = func.call(List.of(arg1, arg2));
+    assertEquals(Tag.CALL, call.tag());
+    assertEquals(func, call.get(0));
+    assertEquals(arg1, call.get(1));
+    assertEquals(arg2, call.get(2));
+  }
+
+  @Test
+  void testIterator() {
+    Term term = Term.intConstant(Type.I1, 5L);
+    Iterator<Term> iterator = term.iterator();
+    assertFalse(iterator.hasNext());
+    assertThrows(NoSuchElementException.class, iterator::next);
+  }
+
+  @Test
+  void testNull() {
+    Term term = Term.NULL;
+    assertEquals(Tag.NULL, term.tag());
+    assertEquals(Type.PTR, term.type());
+  }
+
+  @Test
+  void testEqualsAndHashCode() {
+    Term term1 = Term.intConstant(Type.I1, 5L);
+    Term term2 = Term.intConstant(Type.I1, 5L);
+    Term term3 = Term.intConstant(Type.I1, 6L);
+    assertEquals(term1, term2);
+    assertNotEquals(term1, term3);
+    assertEquals(term1.hashCode(), term2.hashCode());
+    assertNotEquals(term1.hashCode(), term3.hashCode());
+  }
+
+  @Test
+  void testSize() {
+    Term term = Term.intConstant(Type.I1, 5L);
+    assertEquals(0, term.size());
+    Term add = term.add(term);
+    assertEquals(2, add.size());
+  }
+
+  @Test
+  void testGet() {
+    Term term1 = Term.intConstant(Type.I1, 2L);
+    Term term2 = Term.intConstant(Type.I1, 3L);
+    Term add = term1.add(term2);
+    assertEquals(term1, add.get(0));
+    assertEquals(term2, add.get(1));
+    assertThrows(IndexOutOfBoundsException.class, () -> add.get(2));
   }
 }
