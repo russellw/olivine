23d8f9690b67b29292c0328f55f113c83f17ac50
Russell Wallace <russell.wallace@gmail.com>
2024-06-04 22:33:12 +0100

    test

--- test.bat
+++ test.bat
@@ -1,2 +1,4 @@
-call mvn test||exit /b
-java -cp target\classes olivine.Main %*
+rem call mvn test||exit /b
+call mvn compile||exit /b
+java -cp target\classes olivine.Main test.c||exit /b
+type a.ll
