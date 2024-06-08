84c488f20449631ba94ee1a59f0b6cc49efd77e2
Russell Wallace <russell.wallace@gmail.com>
2024-06-08 08:06:40 +0100

    fmt

--- fmt.bat
+++ fmt.bat
@@ -2,4 +2,5 @@ google-java-format -i src/main/java/olivine/*.java
 google-java-format -i src/test/java/olivine/*.java
 black .
 for %%x in (test\*.c) do clang-format -i %%x
+for %%x in (test\*.cpp) do clang-format -i %%x
 git diff
