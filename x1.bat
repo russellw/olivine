ca5cd6ce0f5d847682db2798310d44803f77f971
Russell Wallace <russell.wallace@gmail.com>
2024-06-15 11:01:39 +0100

    test

--- x1.bat
+++ x1.bat
@@ -1,5 +1,5 @@
-rem call mvn test||exit /b
 call mvn compile||exit /b
-clang -emit-llvm -S test.c
-java -cp target\classes olivine.Main test.ll||exit /b
+clang -emit-llvm -S \t\a.c -o a1.ll||exit /b
+java -classpath target\classes -enableassertions olivine.Main a1.ll||exit /b
 type a.ll
+clang a.ll
