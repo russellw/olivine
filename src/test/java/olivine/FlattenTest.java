1bfd6b382b6f2567b17aba5aa2a32b057a196e23
Russell Wallace <russell.wallace@gmail.com>
2024-06-17 16:15:35 +0100

    test

--- src/test/java/olivine/FlattenTest.java
+++ src/test/java/olivine/FlattenTest.java
@@ -26,6 +26,36 @@ class FlattenTest {
 
     assertEquals(2, block.size());
     var assign = (Assign) block.get(0);
+    assertEquals(Type.I32, assign.variable.type());
     assertEquals(Tag.ADD, assign.value.tag());
+  }
+
+  @Test
+  void testFlattenFieldPtr() {
+    // red, green, blue
+    var colorType = Type.struct(Type.FLOAT, Type.FLOAT, Type.FLOAT);
+
+    // foreground, background
+    var colorsType = Type.struct(colorType, colorType);
+
+    var colors = new Variable(colorsType);
+    var function = new Function("f", Type.FLOAT, List.of(colors), false);
+
+    // colors->background->blue
+    var background = colors.fieldPtr(colorsType, 1);
+    var blue = background.fieldPtr(colorType, 2);
+
+    var block = new Block();
+    block.add(new Ret(blue));
+    function.entry = block;
+    var module = new Module();
+    module.functions.add(function);
+
+    Flatten.run(module);
+
+    assertEquals(2, block.size());
+    var assign = (Assign) block.get(0);
+    assertEquals(Type.PTR, assign.variable.type());
+    assertEquals(Tag.FIELD_PTR, assign.value.tag());
   }
 }
