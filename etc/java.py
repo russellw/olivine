e0fc5d4b1c4d075520697ced4779b108e4822be4
Russell Wallace <russell.wallace@gmail.com>
2024-06-20 23:29:19 +0100

    refactor

---
+++ etc/java.py
@@ -0,0 +1,105 @@
+import re
+import etc
+
+
+def src_files():
+    directory = os.path.join("src", "main", "java", "olivine")
+    java_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".java"):
+                file_path = os.path.join(root, file)
+                java_files.append(file_path)
+    return java_files
+
+
+def java(v):
+    i = 0
+
+    # package
+    assert v[i].startswith("package")
+    i += 1
+
+    # import
+    while not v[i] or v[i].startswith("import"):
+        i += 1
+    header = v[:i]
+
+    def parse_member():
+        nonlocal i
+
+        # Comments
+        j = i
+        while re.match(r" *//", v[j]):
+            j += 1
+
+        # Annotations
+        while re.match(r" *@", v[j]):
+            j += 1
+        header = v[i:j]
+        i = j
+
+        # Signature
+        assert re.match(r" *\w", v[i])
+        signature = v[i]
+        i += 1
+
+        # Enum
+        if is_enum_start(signature):
+            a = Enum(header, signature)
+            while not closes(dent, v[i]):
+                a.members.append(v[i])
+                i += 1
+            i += 1
+            return a
+
+        # Class
+        if is_class_start(signature):
+            dent = indentation(signature)
+            a = Class(header, signature)
+            while 1:
+                while not v[i]:
+                    i += 1
+                if closes(dent, v[i]):
+                    break
+                a.members.append(parse_member())
+            i += 1
+            return a
+
+        # Method
+        if re.match(r" *[\w ]*\(.*\) {$", signature):
+            a = Method(header, signature)
+            while not closes(dent, v[i]):
+                a.body.append(v[i])
+                i += 1
+            i += 1
+            return a
+
+    a = parse_member()
+    a.header = header + a.header
+    return a
+
+
+def closes(dent, s):
+    return s == " " * dent + "}"
+
+
+class Class:
+    def __init__(self, header, signature):
+        self.header = header
+        self.signature = signature
+        self.members = []
+
+
+class Method:
+    def __init__(self, header, signature):
+        self.header = header
+        self.signature = signature
+        self.body = []
+
+
+class Enum:
+    def __init__(self, header, signature):
+        self.header = header
+        self.signature = signature
+        self.members = []
