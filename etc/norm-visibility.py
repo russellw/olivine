912e00eb00bd50b7e0a2dcfa9c5b3305c9ce045a
Russell Wallace <russell.wallace@gmail.com>
2024-06-21 15:18:13 +0100

    main

--- etc/norm-visibility.py
+++ etc/norm-visibility.py
@@ -3,7 +3,9 @@ import java
 import etc
 
 for file in java.src_files():
-    print(file)
     v = etc.read_lines(file)
     a = java.parse(v)
-    etc.write_lines(file, java.compose(a))
+    r = java.compose(a)
+    if r != v:
+        print(file)
+        etc.write_lines(file, r)
