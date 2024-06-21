b4986ce967cb3885dbe06988b0b68b4b8f922431
Russell Wallace <russell.wallace@gmail.com>
2024-06-21 23:13:41 +0100

    sort python

---
+++ etc/sort-python.py
@@ -0,0 +1,13 @@
+# Sort Python functions and classes
+# Internal tool, designed for this project only
+# Does NOT work for arbitrary Python code
+import python
+
+import etc
+
+for file in java.src_files():
+    v = etc.read_lines(file)
+    r = java.compose(a)
+    if r != v:
+        print(file)
+        etc.write_lines(file, r)
