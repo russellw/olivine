1254e87ff4673f5ba11da12d2bfd1a47a69de2d2
Russell Wallace <russell.wallace@gmail.com>
2024-06-22 16:33:00 +0100

    case

--- etc/case-comments-python.py
+++ etc/case-comments-python.py
@@ -1,12 +1,20 @@
 # Uppercase first letter of comments
 # Internal tool, designed for this project only
 # Does NOT work for arbitrary Python code
+import re
+
 import etc
 import python
 
+
+def f(s):
+    r = re.match("( *# )([a-z])(.*)", s)
+    if r:
+        return f"{r[1]}{r[2].upper()}{r[3]}"
+    return s
+
+
 for file in python.src_files():
     v = etc.read_lines(file)
-    a = python.parse(v)
-    python.sort(a)
-    v = python.compose(a)
+    v = map(f, v)
     etc.write_lines(file, v)
