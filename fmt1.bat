127bce7af3cb6b97e6f4fec7707b4005f87f008c
Russell Wallace <russell.wallace@gmail.com>
2024-06-23 17:09:23 +0100

    java

--- fmt1.bat
+++ fmt1.bat
@@ -5,5 +5,17 @@ python etc\sort-python.py||exit /b
 black .||exit /b
 isort .||exit /b
 
+rem Java
+python etc\case-comments-java.py||exit /b
+
+python etc\sort-case-labels.py||exit /b
+google-java-format -i src/main/java/olivine/*.java||exit /b
+
+python etc\sort-cases.py||exit /b
+google-java-format -i src/main/java/olivine/*.java||exit /b
+
+python etc\sort-members.py||exit /b
+google-java-format -i src/main/java/olivine/*.java||exit /b
+
 rem Check the results
 git diff
