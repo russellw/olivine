91f510e17ca24008b9015f091c19da44f6bd55e8
Russell Wallace <russell.wallace@gmail.com>
2024-06-28 13:13:12 +0100

    optimize test build

--- x1.bat
+++ x1.bat
@@ -1,5 +1,5 @@
 call mvn compile||exit /b
-clang -emit-llvm -S \t\a.c -o a1.ll||exit /b
+clang -O2 -emit-llvm -S \t\a.c -o a1.ll||exit /b
 type a1.ll
 java -classpath target\classes -enableassertions olivine.Main a1.ll||exit /b
 type a.ll
