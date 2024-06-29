2b5435236f9881b54a63479ef984a624cf71b8b6
Russell Wallace <russell.wallace@gmail.com>
2024-06-29 15:57:24 +0100

    test

--- x1.bat
+++ x1.bat
@@ -1,8 +1,10 @@
 call mvn compile||exit /b
 clang -O2 -emit-llvm -S \t\a.c -o a1.ll||exit /b
 type a1.ll
+clang a1.ll -o a1.exe||exit /b
 java -classpath target\classes -enableassertions olivine.Main a1.ll||exit /b
 type a.ll
 clang a.ll||exit /b
-a
+a1 123
+a 123
 echo %errorlevel%
