066057427ec95a7f5c7101f9e44ac3c180b9c66c
Russell Wallace <russell.wallace@gmail.com>
2025-02-17 05:21:09 +0000

    Modified 1 file(s): f.bat

--- f.bat
+++ f.bat
@@ -1,5 +1,6 @@
 clang-format -i --style=file *.cpp src/*.h src/*.cpp||exit /b
 sort-enums -w *.cpp src/*.h src/*.cpp||exit /b
+sort-blocks -w *.cpp src/*.h src/*.cpp||exit /b
 sort-fns-cpp -w *.cpp src/*.h src/*.cpp||exit /b
 do-all-recur . sort-cases -w||exit /b
 do-all-recur . sort-case-blocks -w||exit /b
