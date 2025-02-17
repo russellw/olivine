64afe93d7f9e39c2fdc2dee3d9c1575654a709d4
Russell Wallace <russell.wallace@gmail.com>
2025-02-17 03:15:10 +0000

    sort enums

--- f.bat
+++ f.bat
@@ -1,4 +1,5 @@
 clang-format -i --style=file *.cpp src/*.h src/*.cpp||exit /b
+sort-enums -w *.cpp src/*.h src/*.cpp||exit /b
 do-all-recur . sort-cases -w||exit /b
 do-all-recur . sort-case-blocks -w||exit /b
 git diff
