e3661e0ee7acaf6b3803cf7973416b218fddb118
Russell Wallace <russell.wallace@gmail.com>
2025-02-19 05:35:59 +0000

    Added 1 file(s): src/instruction.cpp; Modified 1 file(s): src/term.cpp

---
+++ src/instruction.cpp
@@ -0,0 +1,25 @@
+#include "all.h"
+
+struct InstructionImpl {
+	const Opcode opcode;
+	const vector<Term> operands;
+
+	InstructionImpl(Opcode opcode, const vector<Term> &operands): opcode(opcode), operands(operands) {
+	}
+};
+
+Instruction::Instruction(Opcode opcode) {
+	p = new InstructionImpl(opcode, {});
+}
+
+Instruction::Instruction(Opcode opcode,Term a) {
+	p = new InstructionImpl(opcode, {a});
+}
+
+Instruction::Instruction(Opcode opcode,Term a,Term b) {
+	p = new InstructionImpl(opcode, {a,b});
+}
+
+Instruction::Instruction(Opcode opcode,Term a,Term b,Term c) {
+	p = new InstructionImpl(opcode, {a,b,c});
+}
