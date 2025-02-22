397538980adc75b6952cf58af28490d6991bbc5f
Russell Wallace <russell.wallace@gmail.com>
2025-02-22 03:40:38 +0000

    SetUnhandledExceptionFilter

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include "all.h"
 
+#ifdef _WIN32
+#include <windows.h>
+
+LONG WINAPI unhandledExceptionFilter(EXCEPTION_POINTERS* exInfo) {
+	std::cerr << "Unhandled exception: " << std::hex << exInfo->ExceptionRecord->ExceptionCode << '\n';
+	return EXCEPTION_EXECUTE_HANDLER;
+}
+#endif
+
 int main(int argc, char** argv) {
 	try {
+#ifdef _WIN32
+		SetUnhandledExceptionFilter(unhandledExceptionFilter);
+#endif
 		vector<string> files;
 		for (int i = 1; i < argc; i++) {
 			auto s = argv[i];
+			s = 0;
 			if (*s == '-') {
 				while (*s == '-') {
 					s++;
