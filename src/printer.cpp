1df2dee6909b4368827e7ebf614fd7854054146c
Russell Wallace <russell.wallace@gmail.com>
2025-02-22 10:36:07 +0000

    Modified 1 file(s): src/printer.cpp

--- src/printer.cpp
+++ src/printer.cpp
@@ -179,3 +179,93 @@ ostream& operator<<(ostream& os, Term a) {
 	os << ")";
 	return os;
 }
+
+ostream& operator<<(ostream& os, Inst inst) {
+	// Handle empty instructions
+	if (!inst.size()) {
+		switch (inst.opcode()) {
+		case RetVoid:
+			return os << "ret void";
+		case Unreachable:
+			return os << "unreachable";
+		default:
+			ASSERT(false && "Invalid empty instruction");
+		}
+	}
+
+	switch (inst.opcode()) {
+	case Alloca: {
+		// Format: %var = alloca type, i32 count
+		ASSERT(inst.size() == 3);
+		os << inst[0] << " = alloca " << inst[1].ty() << ", i32 " << inst[2];
+		break;
+	}
+	case Assign: {
+		// Format: %var = expr
+		ASSERT(inst.size() == 2);
+		os << inst[0] << " = " << inst[1];
+		break;
+	}
+	case Block: {
+		// Format: label:
+		ASSERT(inst.size() == 1);
+		os << inst[0].ref() << ":";
+		break;
+	}
+	case Br: {
+		// Format: br i1 %cond, label %true, label %false
+		ASSERT(inst.size() == 3);
+		os << "br i1 " << inst[0] << ", label " << inst[1] << ", label " << inst[2];
+		break;
+	}
+	case Drop: {
+		// Format: expr
+		ASSERT(inst.size() == 1);
+		os << inst[0];
+		break;
+	}
+	case Jmp: {
+		// Format: br label %target
+		ASSERT(inst.size() == 1);
+		os << "br label " << inst[0];
+		break;
+	}
+	case Phi: {
+		// Format: %var = phi type [ val1, %label1 ], [ val2, %label2 ], ...
+		ASSERT(inst.size() >= 3 && (inst.size() % 2) == 1);
+		os << inst[0] << " = phi " << inst[1].ty();
+		for (size_t i = 1; i < inst.size(); i += 2) {
+			os << " [ " << inst[i] << ", " << inst[i + 1] << " ]";
+		}
+		break;
+	}
+	case Ret: {
+		// Format: ret type value
+		ASSERT(inst.size() == 1);
+		os << "ret " << inst[0].ty() << " " << inst[0];
+		break;
+	}
+	case Store: {
+		// Format: store type value, type* pointer
+		ASSERT(inst.size() == 2);
+		os << "store " << inst[0].ty() << " " << inst[0] << ", " << inst[1].ty() << "* " << inst[1];
+		break;
+	}
+	case Switch: {
+		// Format: switch type value, label %default [ type val1, label %label1 ... ]
+		ASSERT(inst.size() >= 2 && (inst.size() % 2) == 0);
+		os << "switch " << inst[0].ty() << " " << inst[0] << ", label " << inst[1];
+		if (inst.size() > 2) {
+			os << " [";
+			for (size_t i = 2; i < inst.size(); i += 2) {
+				os << "\n    " << inst[i].ty() << " " << inst[i] << ", label " << inst[i + 1];
+			}
+			os << "\n  ]";
+		}
+		break;
+	}
+	default:
+		ASSERT(false && "Unknown instruction type");
+	}
+	return os;
+}
