d96f1d2b669e4c7e7649ce5ca3258b47a1649987
Russell Wallace <russell.wallace@gmail.com>
2025-02-22 12:52:05 +0000

    Modified 1 file(s): src/printer.h

--- src/printer.h
+++ src/printer.h
@@ -4,10 +4,10 @@
 // A string that begins with a digit, or is otherwise not a valid identifier, is wrapped in quotes
 // and special characters are escaped as a pair of hex digits
 // with the single exception of `\` which is converted to `\\`
-ostream& operator<<(ostream& os, const Ref& ref);
+ostream& operator<<(ostream& os, const Ref&);
 
 // Output a type in LLVM format
-ostream& operator<<(ostream& os, Type ty);
+ostream& operator<<(ostream& os, Type);
 
 // Output a term in LLVM format
 // Compound terms are printed in constant expression format
@@ -15,10 +15,10 @@ ostream& operator<<(ostream& os, Type ty);
 // but extending it to other operators is still useful for debugging
 // Note that when printing instructions, this function can only be used for atomic terms
 // as instructions have a different format for operator with operands
-ostream& operator<<(ostream& os, Term a);
+ostream& operator<<(ostream& os, Term);
 
 // Output an instruction in LLVM format
-ostream& operator<<(ostream& os, Inst inst);
+ostream& operator<<(ostream& os, Inst);
 
 // Output a function in LLVM format
 // with `declare` or `define` depending on whether the function has a body
