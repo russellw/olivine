eecf201087c4ea83ad745f4c403e2a7711318a90
Russell Wallace <russell.wallace@gmail.com>
2025-02-23 08:20:39 +0000

    Modified 2 file(s): src/ssa.cpp, src/ssa.h

--- src/ssa.h
+++ src/ssa.h
@@ -1,2 +1,5 @@
 // Converts LLVM-style phi nodes into equivalent code using mutable local variables
 Func eliminatePhiNodes(const Func& func);
+
+// Convert back to SSA form in preparation for LLVM output
+Func convertToAllocas(const Func& func);
