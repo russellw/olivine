9379f379abc739d39c847a9dab2ed463c8a94531
Russell Wallace <russell.wallace@gmail.com>
2025-02-23 10:19:49 +0000

    Modified 1 file(s): src/context.h

--- src/context.h
+++ src/context.h
@@ -1,3 +1,5 @@
+// Some things, like the target platform, and declarations of external functions
+// are effectively global constants during the operation of the optimizer
 namespace context {
 extern string datalayout;
 extern string triple;
