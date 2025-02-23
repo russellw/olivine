6ab6f7e4055028061281ff1513047e1b16c21d5e
Russell Wallace <russell.wallace@gmail.com>
2025-02-23 05:30:22 +0000

    Modified 1 file(s): src/check.h

--- src/check.h
+++ src/check.h
@@ -2,12 +2,18 @@
 // for example, that all operands have valid types for the operation
 // and throw an exception if not
 // This function is not recursive; it only checks the top level of a compound type
-void check(Term a);
+void check(Term);
 
 // Recursive checker function that validates a term and all its nested subterms
-void checkRecursive(Term a);
+void checkRecursive(Term);
 
 // Check that an instruction is consistent
 // Run checkRecursive on all operands
 // then perform additional checks to make sure every operand is valid for the instruction
-void check(Inst inst);
+void check(Inst);
+
+// Check that a function definition is consistent
+// Olivine does not use phi internally
+// and this is meant to be used after converting freshly parsed modules to internal form
+// so also checks for the absence of phi
+void check(Function);
