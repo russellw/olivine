0b6e0e7e76a28869ab44cfc1213b63d62f663e6a
Russell Wallace <russell.wallace@gmail.com>
2025-02-27 09:25:40 +0000

    Modified 1 file(s): integration-tests/add/test.py

--- integration-tests/add/test.py
+++ integration-tests/add/test.py
@@ -1,10 +1,11 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+current_dir = os.path.dirname(__file__)
+sys.path.append(os.path.dirname(current_dir))
 from common.etc import *
 
-src = os.path.join(os.path.dirname(__file__), "add.c")
+src = os.path.join(current_dir, "add.c")
 clang("-emit-llvm -S " + src)
 olivine("add.ll")
 clang("-c a.ll")
