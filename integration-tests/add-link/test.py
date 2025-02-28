9f53bf4792b7b56828af77b3d8cf3c6a9a673bfc
Russell Wallace <russell.wallace@gmail.com>
2025-02-28 23:09:21 +0000

    Added 1 file(s): integration-tests/add-link/main.c; Modified 1 file(s): integration-tests/add-link/test.py

--- integration-tests/add-link/test.py
+++ integration-tests/add-link/test.py
@@ -6,15 +6,15 @@ current_dir = os.path.dirname(__file__)
 sys.path.append(os.path.dirname(current_dir))
 from common.etc import *
 
-# Simple test with one source file
-name = os.path.basename(current_dir)
-src = os.path.join(current_dir, name + ".c")
-
 # Compile to .ll
-clang("-emit-llvm -S " + src)
+clang("-emit-llvm -S " + os.path.join(current_dir, "add.c"))
+clang("-emit-llvm -S " + os.path.join(current_dir, "main.c"))
 
 # Process
-olivine(name + ".ll")
+olivine("add.ll main.ll")
 
-# Make sure the output is at least valid LLVM
-clang("-c a.ll")
+# Compile to an executable
+clang("a.ll")
+
+# Check result
+check_return_code("a", (), 7)
