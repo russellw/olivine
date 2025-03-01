c72fd675d85f6c7a0307434a69746bc9459dde42
Russell Wallace <russell.wallace@gmail.com>
2025-03-01 21:36:53 +0000

    Added 2 file(s): integration-tests/template-comdat/template-comdat.cpp, integration-tests/template-comdat/test.py

---
+++ integration-tests/template-comdat/test.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+# Import common functions from ../common
+current_dir = os.path.dirname(__file__)
+sys.path.append(os.path.dirname(current_dir))
+from common.etc import *
+
+# Simple test with one source file
+name = os.path.basename(current_dir)
+src = os.path.join(current_dir, name + ".cpp")
+
+# Compile to .ll
+clang("-emit-llvm -S " + src)
+
+# Process
+olivine(name + ".ll")
+
+# Compile to an executable
+clang("a.ll")
+
+# Check result
+check_return_code("a", (), 143)
