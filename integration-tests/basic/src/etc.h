18b5043182e0e9f252b7d03428c85112b068df55
Russell Wallace <russell.wallace@gmail.com>
2025-03-06 19:05:38 +0000

    Added 1 file(s): integration-tests/basic/unit-tests/map-multi.cpp; Modified 1 file(s): integration-tests/basic/src/etc.h

--- integration-tests/basic/src/etc.h
+++ integration-tests/basic/src/etc.h
@@ -23,6 +23,8 @@ template <typename T> size_t hashVector(const vector<T>& v) {
 	return hashRange(v.begin(), v.end());
 }
 
+//Let T be an element type and F a function T->std::invoke_result_t<F, T>
+//Map F over each element of an input vector and return the result
 template <typename T, typename F> vector<std::invoke_result_t<F, T>> map(const vector<T>& input, F func) {
 	vector<std::invoke_result_t<F, T>> result;
 	result.reserve(input.size());
@@ -31,6 +33,28 @@ template <typename T, typename F> vector<std::invoke_result_t<F, T>> map(const v
 
 	return result;
 }
+
+
+//Let T be an element type and F a function T->vector<T>
+//Map F over each element of an input vector and return the concatenated results
+template <typename T, typename F> vector<T> mapMulti(const vector<T>& input, F func) {
+vector<T> result;
+    
+    // Pre-allocate memory for the result vector
+    // This is an optimization to avoid frequent reallocations
+    // If we knew the sizes of all output vectors in advance, we could be more precise here
+    result.reserve(input.size()); // Start with at least as many elements as the input
+    
+    // For each element in the input vector
+    for (const auto& element : input) {
+        // Apply the function to get a vector result
+        vector<T> subResult = func(element);
+        
+        // Append the elements of subResult to the result vector
+        result.insert(result.end(), subResult.begin(), subResult.end());
+    }
+    
+    return result;}
 
 template <class K, class V> ostream& operator<<(ostream& os, const unordered_map<K, V>& m) {
 	os << '{';
