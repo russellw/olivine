9c97cedae07209b7b83929ab4f3db1ce700d3f26
Russell Wallace <russell.wallace@gmail.com>
2025-03-06 15:33:01 +0000

    Comparison operator doesn't need to be inline

--- src/ref.cpp
+++ src/ref.cpp
@@ -3,3 +3,19 @@
 bool Ref::operator==(const Ref& b) const {
 	return num1 == b.num1 && str1 == b.str1;
 }
+
+bool Ref::operator<(const Ref& b) const {
+	// First compare by which type
+	if (numeric() != b.numeric()) {
+		return numeric() < b.numeric();
+	}
+
+	// Same type, now compare values
+	if (numeric()) {
+		// Both are size_t
+		return num() < b.num();
+	} else {
+		// Both are string
+		return str() < b.str();
+	}
+}
--- src/ref.h
+++ src/ref.h
@@ -37,21 +37,7 @@ public:
 		return !(*this == b);
 	}
 
-	bool operator<(const Ref& b) const {
-		// First compare by which type
-		if (numeric() != b.numeric()) {
-			return numeric() < b.numeric();
-		}
-
-		// Same type, now compare values
-		if (numeric()) {
-			// Both are size_t
-			return num() < b.num();
-		} else {
-			// Both are string
-			return str() < b.str();
-		}
-	}
+	bool operator<(const Ref& b) const;
 };
 
 namespace std {
