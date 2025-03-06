da87ddb1a75d1f7d84655ca260fc856023f7864c
Russell Wallace <russell.wallace@gmail.com>
2025-03-06 16:45:40 +0000

    Added 6 file(s): basic/generate-ninja.py, basic/main.cpp, basic/src/all.h, ...and 3 more

---
+++ basic/main.cpp
@@ -0,0 +1,83 @@
+#include "all.h"
+
+#ifdef _WIN32
+#include <windows.h>
+
+LONG WINAPI unhandledExceptionFilter(EXCEPTION_POINTERS* exInfo) {
+	cerr << "Unhandled exception: " << std::hex << exInfo->ExceptionRecord->ExceptionCode << '\n';
+	return EXCEPTION_EXECUTE_HANDLER;
+}
+#endif
+
+static char* optArg(int argc, char** argv, int& i, char* s) {
+	if (s[1]) {
+		return s + 1;
+	}
+	if (i + 1 == argc) {
+		throw runtime_error(string(argv[i]) + ": expected arg");
+	}
+	++i;
+	return argv[i];
+}
+
+static string readFile(string filename) {
+	std::ifstream file(filename, std::ios::binary | std::ios::ate);
+	if (!file) {
+		// Get the error code and create a system_error with the OS error message
+		std::error_code ec(errno, std::system_category());
+		throw std::system_error(ec, "Failed to open file: " + filename);
+	}
+
+	std::streamsize size = file.tellg();
+	file.seekg(0, std::ios::beg);
+
+	string content(size, '\0');
+	if (!file.read(&content[0], size)) {
+		std::error_code ec(errno, std::system_category());
+		throw std::system_error(ec, "Failed to read file: " + filename);
+	}
+
+	return content;
+}
+
+int main(int argc, char** argv) {
+	try {
+#ifdef _WIN32
+		SetUnhandledExceptionFilter(unhandledExceptionFilter);
+#endif
+		string file;
+		for (int i = 1; i < argc; i++) {
+			auto s = argv[i];
+			if (*s == '-') {
+				while (*s == '-') {
+					s++;
+				}
+				switch (*s) {
+				case 'V':
+				case 'v':
+					cout << "Olivine Basic\n";
+					return 0;
+				case 'h':
+					cout << "Usage: basic [options] file.bas\n";
+					cout << "\n";
+					cout << "-h  Show help\n";
+					cout << "-V  Show version\n";
+					return 0;
+				}
+				throw runtime_error(string(argv[i]) + ": unknown option");
+			}
+			file=s;
+		}
+		if (file.empty()) {
+			throw runtime_error("No input file");
+		}
+			auto text = readFile(file);
+		return 0;
+	} catch (const std::exception& e) {
+		cerr << e.what() << '\n';
+		return 1;
+	} catch (...) {
+		cerr << "Unknown exception\n";
+		return 1;
+	}
+}
---
+++ basic/src/etc.h
@@ -0,0 +1,70 @@
+#define dbg(a) cout << __FILE__ << ':' << __LINE__ << ": " << (a) << '\n'
+
+#define ASSERT(cond)                                                                          \
+	do {                                                                                      \
+		if (!(cond)) {                                                                        \
+			throw runtime_error(string(__FILE__) + ':' + to_string(__LINE__) + ": " + #cond); \
+		}                                                                                     \
+	} while (0)
+
+void stackTrace(std::ostream& out = std::cout);
+
+// SORT FUNCTIONS
+
+template <typename T> vector<T> cons(const T& x, const vector<T>& xs) {
+	vector<T> result;
+	result.reserve(1 + xs.size());
+	result.push_back(x);
+	result.insert(result.end(), xs.begin(), xs.end());
+	return result;
+}
+
+template <typename Iterator> size_t hashRange(Iterator first, Iterator last) {
+	size_t h = 0;
+	for (; first != last; ++first) {
+		hash_combine(h, hash<typename Iterator::value_type>()(*first));
+	}
+	return h;
+}
+
+template <typename T> size_t hashVector(const vector<T>& v) {
+	return hashRange(v.begin(), v.end());
+}
+
+template <typename T, typename F> vector<std::invoke_result_t<F, T>> map(const vector<T>& input, F func) {
+	vector<std::invoke_result_t<F, T>> result;
+	result.reserve(input.size());
+
+	std::transform(input.begin(), input.end(), std::back_inserter(result), func);
+
+	return result;
+}
+
+template <class K, class V> ostream& operator<<(ostream& os, const unordered_map<K, V>& m) {
+	os << '{';
+	for (auto i = begin(m); i != end(m); ++i) {
+		if (i != begin(m)) {
+			os << ", ";
+		}
+		os << i->first << ':' << i->second;
+	}
+	return os << '}';
+}
+
+template <class T> ostream& operator<<(ostream& os, const vector<T>& v) {
+	os << '[';
+	for (auto i = begin(v); i != end(v); ++i) {
+		if (i != begin(v)) {
+			os << ", ";
+		}
+		os << *i;
+	}
+	return os << ']';
+}
+
+template <typename T> std::vector<T> tail(const std::vector<T>& vec) {
+	if (vec.empty()) {
+		return std::vector<T>();
+	}
+	return std::vector<T>(vec.begin() + 1, vec.end());
+}
