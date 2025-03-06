fe96ffacba8d9b0a122d88d22ec03cd1e0b87b89
Russell Wallace <russell.wallace@gmail.com>
2025-03-06 18:20:59 +0000

    Modified 3 file(s): integration-tests/basic/main.cpp, integration-tests/basic/src/parser.cpp, integration-tests/basic/src/parser.h

--- integration-tests/basic/main.cpp
+++ integration-tests/basic/main.cpp
@@ -20,6 +20,27 @@ static char* optArg(int argc, char** argv, int& i, char* s) {
 	return argv[i];
 }
 
+// Read a file into a vector of strings, one per line
+static vector<string> readLines(string file) {
+	std::ifstream inFile(file);
+	if (!inFile.is_open()) {
+		throw runtime_error("Failed to open file: " + file);
+	}
+
+	vector<string> lines;
+	string line;
+	while (std::getline(inFile, line)) {
+		lines.push_back(line);
+	}
+
+	if (inFile.bad()) {
+		throw runtime_error("Error while reading file: " + file);
+	}
+
+	inFile.close();
+	return lines;
+}
+
 int main(int argc, char** argv) {
 	try {
 #ifdef _WIN32
--- integration-tests/basic/src/parser.h
+++ integration-tests/basic/src/parser.h
@@ -1,12 +1,46 @@
-// Read a file into a vector of strings, one per line
-vector<string> readLines(string file);
-
 // Given one line of Basic, remove the comment if there is one
 // Make sure to avoid false positive in case REM is a substring of a longer word
 // or in case it is within a quoted string
 string removeComment(string);
 
-// Given a Basic program where lines can contain colons
-// split it up into strictly one statement per line
-// making sure to avoid splitting on colons that are within comments or quoted strings
+// A line of Basic code may optionally have a label
+// This may consist of one or more digits
+// or a letter or underscore, followed by zero or more letters, digits or underscores
+struct Line {
+	string label;
+	string text;
+
+	Line(string label, string text): label(label), text(text) {
+	}
+};
+
+// Parse a line of Basic code to extract the label
+// This may consist of one or more digits
+// or a letter or underscore, followed by zero or more letters, digits or underscores, and a colon
+// In the latter case, the colon is not stored in the label
+// The remainder of the line, after the label, is stored in the text field
+Line parseLabel(string);
+
+/*
+Quoted strings make it more difficult to parse Basic
+Factor them out into special variables
+assigned at the start of the program
+whose names begin with STRING_LITERAL_
+For example, given input:
+
+10 PRINT "FOO"+"BAR"
+
+this function returns:
+
+LET STRING_LITERAL_0$ = "FOO"
+LET STRING_LITERAL_1$ = "FOO"
+10 PRINT STRING_LITERAL_0+STRING_LITERAL_1
+*/
+vector<string> splitColons(vector<string>);
+
+/*
+Given a Basic program where lines can contain colons
+split it up into strictly one statement per line
+making sure to avoid splitting on colons that are within comments or quoted strings
+*/
 vector<string> splitColons(vector<string>);
